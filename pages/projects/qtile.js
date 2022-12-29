aea5c6dd4e787169801a13f5c2f6754e53f45fd2
RonitGandhi-ArchLinux <ronitgandhi96@gmail.com>
2022-12-29 10:42:06 +0530

    qtile page

---
+++ pages/projects/qtile.js
@@ -0,0 +1,62 @@
+import { Container, Link, List, ListItem, Badge } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Title, WorkImage, Meta } from "../../components/work";
+import P from "../../components/paragraph";
+import Layout from "../../components/layouts/article";
+
+const Work = () => (
+	<Layout title="Qtile">
+		<Container>
+			<Title>Qtile Config</Title>
+			<P>
+				Qtile is a window manager written in python. Qtile is simple,
+				small and extensible. It's easy to write your own layouts,
+				widgets and commands. This is a Qtile config 'riced' as in
+				powerline style.
+			</P>
+			<br />
+			<List ml={4} my={4}>
+				<ListItem>
+					<Meta>Source Code</Meta>
+					<Link
+						href="https://github.com/ronit18/qtile"
+						target="_blank"
+					>
+						Qtile <ExternalLinkIcon mx="2px" />
+					</Link>
+				</ListItem>
+
+				<ListItem>
+					<Meta>Technologies</Meta>
+					<span>
+						<Badge>Python</Badge>
+						&nbsp;
+					</span>
+				</ListItem>
+			</List>
+			<br />
+
+			<WorkImage
+				placeholder="blur"
+				src="/images/projects/netflixhome.gif"
+				alt="qtilemain"
+			/>
+
+			<WorkImage
+				placeholder="blur"
+				src="/images/projects/netflixsignin.png"
+				alt="qtile"
+			/>
+
+			<WorkImage
+				placeholder="blur"
+				src="/images/projects/netflixsignup.png "
+				alt="netflixsignin"
+			/>
+		</Container>
+		<br />
+	</Layout>
+);
+
+export default Work;
+export { getServerSideProps } from "../../components/chakra";
