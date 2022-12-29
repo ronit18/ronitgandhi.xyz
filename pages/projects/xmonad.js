5f143c43bb254f1f20e5b5ff37c014f9b0bb89d2
RonitGandhi-ArchLinux <ronitgandhi96@gmail.com>
2022-12-29 10:41:52 +0530

    xmonad page

---
+++ pages/projects/xmonad.js
@@ -0,0 +1,48 @@
+import { Container, Link, List, ListItem, Badge } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Title, WorkImage, Meta } from "../../components/work";
+import P from "../../components/paragraph";
+import Layout from "../../components/layouts/article";
+
+const Work = () => (
+	<Layout title="Xmonad">
+		<Container>
+			<Title>Xmonad Config</Title>
+			<P>
+				Xmonad is a window manager written in haskell. This is a Xmonad
+				config 'riced'.
+			</P>
+			<br />
+			<List ml={4} my={4}>
+				<ListItem>
+					<Meta>Source Code</Meta>
+					<Link
+						href="https://github.com/ronit18/xmonad"
+						target="_blank"
+					>
+						Xmonad <ExternalLinkIcon mx="2px" />
+					</Link>
+				</ListItem>
+
+				<ListItem>
+					<Meta>Technologies</Meta>
+					<span>
+						<Badge>Haskell</Badge>
+						&nbsp;
+					</span>
+				</ListItem>
+			</List>
+			<br />
+
+			<WorkImage
+				placeholder="blur"
+				src="/images/projects/xmonad.gif"
+				alt="xmonad"
+			/>
+		</Container>
+		<br />
+	</Layout>
+);
+
+export default Work;
+export { getServerSideProps } from "../../components/chakra";
