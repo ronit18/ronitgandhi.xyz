95fe80c821c63ab1c3e0141071dcd828e02445cc
RonitGandhi <ronitgandhi96@gmail.com>
2023-01-30 12:30:35 +0530

    google anaytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
+import Script from 'next/script'
 
 export default class Document extends NextDocument {
   render() {
@@ -12,16 +13,23 @@ export default class Document extends NextDocument {
           <Main />
           <NextScript />
 
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-NMT8DXK7QP"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
 
-            gtag('config', 'G-NMT8DXK7QP');
-          </script>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-NMT8DXK7QP"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NMT8DXK7QP');
+  `}
+          </Script>
         </body>
       </Html>
     )
   }
 }
+
