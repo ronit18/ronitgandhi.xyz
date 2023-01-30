a3b6e8def285dd2b35c79b6d7a69cab5f9025960
RonitGandhi <ronitgandhi96@gmail.com>
2023-01-30 12:23:20 +0530

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,15 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
+
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-NMT8DXK7QP"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NMT8DXK7QP');
+          </script>
         </body>
       </Html>
     )
