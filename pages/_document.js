e8ff109180453f14f59858baed6340634e439d39
Ronit Gandhi <ronitgandhi96@gmail.com>
2023-04-24 23:23:47 +0530

    removed google analytics and gif

--- pages/_document.js
+++ pages/_document.js
@@ -1,35 +1,20 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../lib/theme'
-import Script from 'next/script'
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import theme from "../lib/theme";
 
 export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <Main />
-          <NextScript />
-
-
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-NMT8DXK7QP"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-NMT8DXK7QP');
-  `}
-          </Script>
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head />
+				<body>
+					<ColorModeScript
+						initialColorMode={theme.config.initialColorMode}
+					/>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
-
